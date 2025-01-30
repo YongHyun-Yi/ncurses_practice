05f0ed98ca4981f1c81f5b705c4dbe810502018e
YongHyun Yi <dldydgus1995@naver.com>
2025-01-30 21:58:13 +0900

    feat: simple input and output

---
+++ practice/up_and_down/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string st;
+	std::cout << "Enter the string you want: ";
+	std::cin >> st;
+	std::cout << "your input is: " << st << "\n";
+}
