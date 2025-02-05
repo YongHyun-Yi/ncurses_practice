10c09bf2b91f31200237a774b0dc1bd1659ca292
YongHyun Yi <dldydgus1995@naver.com>
2025-02-05 16:40:51 +0900

    feat: simple guessing game

--- practice/up_and_down/main.cpp
+++ practice/up_and_down/main.cpp
@@ -3,11 +3,31 @@
 #include <time.h>
 #include <stdlib.h>
 
+#include <sstream>
+
 int main()
 {
 	int ans;
 	srand(time(0));
 	ans = rand() % 100 + 1;
 
-	std::cout << "answer: " << ans << "\n";
+	std::cout << "answer: " << ans << "\n\n";
+	while (1)
+	{
+		std::string str;
+		std::stringstream ss;
+		int input;
+
+		std::cout << "guessing: ";
+		std::cin >> str;
+		ss << str;
+		ss >> input;
+
+		if (input == ans)
+		{
+			std::cout << "\ncorrect!\n";
+			break ;
+		}
+		std::cout << "wrong...try again!\n\n";
+	}
 }
